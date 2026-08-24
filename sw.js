b8fbb1ff1056acb415b968e5816cbe89361ddd9b
Limadauto <limadauto@gmail.com>
2026-08-23 21:34:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='eu-vou-sao-silverio-v3';const ASSETS=['./','./index.html','./manifest.json','./template-overlay.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='eu-vou-sao-silverio-v6';
+const ASSETS=['./','./index.html','./manifest.json','./template-overlay.png'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res}).catch(()=>caches.match(event.request)))})
